0c9b6466dd94abd628e7e726b73bdaf6c6e6de85
Aurelien <aurelien.benoist@yahoo.fr>
2020-05-06 20:24:17 -0400

    first version

---
+++ recon.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+
+if [ "$#" -ne 2 ] && [ "$1" != "-h" ] && [ "$1" != "--help" ] && [ "$3" != "-s" ]; then
+  echo "Reconnaissance tool for Penetration Testing\n"
+  echo
+  echo "Syntax: $0 <Host Name> <Working Directory>" >&2
+  echo
+  exit 1
+fi
+
+if ! [ -d "$2" ] && [ "$1" != "-h" ] && [ "$1" != "--help" ]; then
+  echo "$2 not a directory!" >&2
+  exit 1
+fi
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "Reconnaissance tool for Penetration Testing\n"
+  echo
+  echo "Syntax: $0 <Host Name> <Working Directory>"
+  echo "Option:"
+  echo "[-h --help]	Print this help\n"
+  echo "[-s]		Use shodan, you need to specify your API key:\n"
+  echo "	Syntax: $0 <Host Name> <Working Directory> -s <API Key>"
+  echo
+  exit 0
+fi
+
+neofetch	
+hostName=$1
+workingDir=$2
+dirOutput=$workingDir"/"$hostName"_recon/"
+dimOutput=$hostName"_dmitry.out"
+emailAddr=$hostName"_email.txt"
+subdom=$hostName"_subdomains.txt"
+niktoFile=$hostName"_nikto.txt"
+port=80
+
+mkdir $dirOutput
+
+if [ "$3" = "-s" ]; then
+  shodan init $4
+  shodan host $hostName >> $dirOutput/$hostName"_shodan_res.txt"
+fi
+
+
+
+echo "########################\n   nmap in process...\n########################\n"
+nmap -A -T4 -p- $hostName >> $dirOutput/nmap_recon.out
+
+echo "########################\n   dmitry and nslookup\n      in process...\n########################\n"
+echo "########################\n   dmitry Output \n########################\n" > $dirOutput/$dimOutput
+echo "########################\n   Emails found  \n########################\n" >> $dirOutput/$emailAddr
+echo "########################\n   Subdomains  \n########################\n" >> $dirOutput/$subdom
+dmitry -iwnse $hostName | grep "@" >> $dirOutput/$emailAddr
+dmitry -iwnse $hostName | grep "Host" >> $dirOutput/$subdom
+dmitry -iwnse $hostName >> $dirOutput/$dimOutput
+echo "\n########################\n   nslookup Output \n########################\n">> $dirOutput/$dimOutput
+nslookup $hostName >> $dirOutput/$dimOutput
+
+echo "########################\n   nikto in process...\n########################\n"
+# Put statement to change port if p=443!
+nikto -host $hostName -p $port >> $dirOutput/$niktoFile
